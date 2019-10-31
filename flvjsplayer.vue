1a6fbc85979fd348a86e76b1760cdd5b6b28387b
adong <adong@cstor.cn>
2019-10-31 10:55:24 +0800

    Added webgl panorama dev version

--- flvjsplayer.vue
+++ flvjsplayer.vue
@@ -286,6 +286,9 @@ export default {
                 this._closePlayer();
             }
         },
+        getVideoElement() {
+            return this.$refs.videoel;
+        },
         onBeforeLeave() {
             this._closePlayer();
         },
